a3e9c8673bcec9b755c21edf38277c9caa2e9040
Arda Aliz <aaliz19@ku.edu.tr>
2023-01-09 03:23:06 +0300

    Make design of workout details in stroyboard

---
+++ FitJourney/Controller/WorkoutDetailsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  WorkoutDetailsViewController.swift
+//  FitJourney
+//
+//  Created by Arda Aliz on 9.01.2023.
+//
+
+import UIKit
+
+class WorkoutDetailsViewController: UIViewController {
+
+    
+    
+    var section: Int?
+    var row: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- FitJourney/Controller/WorkoutsViewController.swift
+++ FitJourney/Controller/WorkoutsViewController.swift
@@ -28,15 +28,21 @@ class WorkoutsViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if let cell = sender as? UITableViewCell,
+           let indexPath = workoutsTableView.indexPath(for: cell),
+           let workoutDetailsController = segue.destination as? WorkoutDetailsViewController {
+            workoutDetailsController.section = indexPath.section
+            workoutDetailsController.row = indexPath.row
+        }
     }
-    */
+    
 
 }
 
